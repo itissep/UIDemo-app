aeaff35b3f05a2a255721fdc39ea6ee8ff5a0aa4
itissep <someonenewish@yandex.ru>
2026-02-14 17:03:11 +0300

    update project structure

--- LifeCycleDemo/SceneDelegate.swift
+++ LifeCycleDemo/SceneDelegate.swift
@@ -3,11 +3,11 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    weak var demoVC: ViewController?
+    weak var demoVC: AppLifeCycleViewController?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // ...
-        let vc = ViewController()
+        let vc = AppLifeCycleViewController()
         demoVC = vc  // Сохраняем ссылку
         window?.rootViewController = UINavigationController(rootViewController: vc)
     }
@@ -18,31 +18,31 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-        demoVC?.updateBackground(.systemGray)  // Используем!
+        demoVC?.updated(with: .disconnected)
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-        demoVC?.updateBackground(.systemGreen)  // Используем!
+        demoVC?.updated(with: .active)
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
-        demoVC?.updateBackground(.systemYellow)  // Используем!
+        demoVC?.updated(with: .inactive)
     }
 
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
-        demoVC?.updateBackground(.systemYellow)  // Используем!
+        demoVC?.updated(with: .inactive)
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-        demoVC?.updateBackground(.systemBlue)  // Используем!
+        demoVC?.updated(with: .background)
     }
 }
